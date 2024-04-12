c8304af5488988fa9d1ce275a82537d267471a77
keyurparalkar <keur.plkar@gmail.com>
2024-04-12 15:47:01 +0530

    feat: added initial parsing of schema

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
+import { useForm } from "react-hook-form";
 import InputField from "./components/fields/InputField";
+import personalInfoSchema from "./schemas/personal_info.json";
 
 function App() {
+	console.log({ personalInfoSchema });
+	const schema = personalInfoSchema;
+	const { register, handleSubmit } = useForm();
+
+	const onSubmit = (data) => {
+		console.log("form Data = ", data);
+	};
+
 	return (
 		<div className="container mx-auto flex flex-col items-center">
 			<h1 className="text-4xl font-bold tracking-tight mb-5">JSON to Form</h1>
-			<InputField type="text" labelText="First Name" htmlFor="firstName" />
+
+			<form onSubmit={handleSubmit(onSubmit)}>
+				{/* TODO(Keyur): Make the below logic recursive*/}
+				{schema &&
+					schema.fields.map((field) => (
+						<InputField
+							key={`key-${field.accessorKey}`}
+							register={register}
+							labelText={field.fieldName}
+							htmlFor={field.accessorKey}
+							type={field.type}
+						/>
+					))}
+				<input type="submit" />
+			</form>
 		</div>
 	);
 }
--- src/components/fields/InputField.tsx
+++ src/components/fields/InputField.tsx
@@ -1,18 +1,21 @@
+import { UseFormRegister } from "react-hook-form";
 import { Input, InputProps } from "../ui/input";
 import { Label } from "../ui/label";
+import { FieldType } from "@/interfaces/field";
 
 interface InputFieldProps extends InputProps {
 	labelText: string;
 	htmlFor: string;
+	register: UseFormRegister<any>;
 }
 
 const InputField = (props: InputFieldProps) => {
-	const { htmlFor, labelText, ...rest } = props;
+	const { htmlFor, register, labelText, ...rest } = props;
 	return (
 		<>
 			<div className="flex flex-col space-y-2">
 				<Label htmlFor={htmlFor}>{labelText}</Label>
-				<Input id="htmlFor" {...rest} />
+				<Input id="htmlFor" {...rest} {...register(htmlFor)} />
 			</div>
 		</>
 	);
