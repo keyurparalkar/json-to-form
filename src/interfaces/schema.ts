838a7ee4d2a974186ff6636f502c461668d43680
keyurparalkar <keur.plkar@gmail.com>
2024-04-13 16:05:43 +0530

    feat: added zod schema validation

--- src/interfaces/schema.ts
+++ src/interfaces/schema.ts
@@ -1,3 +1,22 @@
-import { GroupField } from "./group";
+import { z } from "zod";
 
-export type SchemaType = GroupField;
+export const FieldTypeDef = z.object({
+	type: z.literal("field"),
+	dataType: z.enum(["text", "number", "email"]),
+	fieldName: z.string(),
+	accessorKey: z.string(),
+});
+
+type GroupFieldType = {
+	type: "group";
+	fields: (z.infer<typeof FieldTypeDef> | GroupFieldType)[];
+};
+
+export const GroupTypeDef: z.ZodType<GroupFieldType> = z.object({
+	type: z.literal("group"),
+	fields: z.array(z.union([FieldTypeDef, z.lazy(() => GroupTypeDef)])),
+});
+
+export const SchemaDef = GroupTypeDef;
+
+export type SchemaType = z.infer<typeof SchemaDef>;
--- src/schemas/forms/personal_info.d.json.ts
+++
@@ -1,5 +0,0 @@
-import { SchemaType } from "@/interfaces/schema";
-
-declare const personal_info: SchemaType;
-
-export default personal_info;
